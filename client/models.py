2056499a62a2cef69e1a41426aa8cf13394076ee
razvantr <razvantrandafirosu@gmail.com>
2023-09-11 20:22:15 +0300

    Models

--- client/models.py
+++ client/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from ride.models import Ride
+
 
 # Create your models here.
+class Client(models.Model):
+
+    name = models.CharField(max_length=60)
+    ride = models.ForeignKey(Ride, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- ride/models.py
+++ ride/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from driver.models import Driver
 from end_location.models import EndLocation
 from start_location.models import StartLocation
 
@@ -8,7 +9,9 @@ class Ride(models.Model):
 
     starting_at = models.ForeignKey(StartLocation, on_delete=models.CASCADE)
     ending_at = models.ForeignKey(EndLocation, on_delete=models.CASCADE)
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
     time = models.DateTimeField()
+    price = models.IntegerField()
 
     def __str__(self):
         return f'From {self.starting_at} to {self.ending_at}, at {self.time}'
