b22510a0cbced74461587ecb72bb8f37325b47eb
razvantr <razvantrandafirosu@gmail.com>
2023-09-11 17:41:40 +0300

    Apps added

--- Final_Project/settings.py
+++ Final_Project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
+    'driver',
+    'car',
+    'start_location',
+    'end_location',
+    'ride',
+    'client',
+    'userextend',
 ]
 
 MIDDLEWARE = [
@@ -117,8 +125,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR), 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Upload media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
---
+++ start_location/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StartLocationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'start_location'
